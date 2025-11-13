1501a6fb02a17ef34559cc3b644a986b59c64659
Neo Fu <rtugeek@gmail.com>
2025-11-13 15:01:30 +0800

    feat: add Redis configuration and integrate RedisModule into the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
+import { RedisModule } from '@liaoliaots/nestjs-redis'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ScheduleModule } from '@nestjs/schedule'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { mysqlConfig } from './db/mysql'
+import { redisConfig } from './db/redis'
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: mysqlConfig,
-  })],
+  imports: [
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: redisConfig,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: mysqlConfig,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/db/redis.ts
@@ -0,0 +1,13 @@
+import { RedisModuleOptions } from '@liaoliaots/nestjs-redis/dist/redis/interfaces/redis-module-options.interface'
+import { ConfigService } from '@nestjs/config'
+
+export function redisConfig(configService: ConfigService): RedisModuleOptions {
+  return {
+    config: {
+      host: configService.get<string>('REDIS_HOST'),
+      port: configService.get<number>('REDIS_PORT', 3306),
+      password: configService.get<string>('REDIS_PASSWORD'),
+      db: configService.get<number>('REDIS_DB', 0),
+    },
+  }
+}
